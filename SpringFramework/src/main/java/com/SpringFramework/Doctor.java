28c4994cacc148c3d7b3fb8d80c719ecfd6240d6
collins <collinsboniface1000@gmail.com>
2022-04-17 11:39:39 +0300

    Added the property with the getter an setter method and toString method

--- SpringFramework/src/main/java/com/SpringFramework/Doctor.java
+++ SpringFramework/src/main/java/com/SpringFramework/Doctor.java
@@ -3,11 +3,27 @@ package com.SpringFramework;
 
 import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class Doctor implements Staff {
+
+    private String qualification;
 
     public void assist() {
         System.out.println("Doctor is Assisting");
     }
 
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "qualification='" + qualification + '\'' +
+                '}';
+    }
 }
