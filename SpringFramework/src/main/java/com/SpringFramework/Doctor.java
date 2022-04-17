9d3b7abfbcfc768f290408af1ae6dcb396166ee4
collins <collinsboniface1000@gmail.com>
2022-04-17 09:51:39 +0300

    Used the Annotation configuration based to let spring create the bean for our Component

--- SpringFramework/src/main/java/com/SpringFramework/Doctor.java
+++ SpringFramework/src/main/java/com/SpringFramework/Doctor.java
@@ -1,29 +1,13 @@
 package com.SpringFramework;
 
-public class Doctor implements Staff {
 
-    private String qualification;
-    private Nurse nurse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Doctor implements Staff {
 
     public void assist() {
         System.out.println("Doctor is Assisting");
-    }
-
-    // Getter and Setter Method
-    public String getQualification() {
-        return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }
-
-    public Nurse getNurse() {
-        return nurse;
-    }
-
-    public void setNurse(Nurse nurse) {
-        this.nurse = nurse;
     }
 
 }
