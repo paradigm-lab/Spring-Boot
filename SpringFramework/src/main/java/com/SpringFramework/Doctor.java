605ae60b8b98635014225bedd1ce4eada1c554c3
collins <collinsboniface1000@gmail.com>
2022-04-17 09:27:43 +0300

    Created property with the getter and setter method

--- SpringFramework/src/main/java/com/SpringFramework/Doctor.java
+++ SpringFramework/src/main/java/com/SpringFramework/Doctor.java
@@ -2,7 +2,19 @@ package com.SpringFramework;
 
 public class Doctor implements Staff {
 
+    private String qualification;
+
     public void assist() {
         System.out.println("Doctor is Assisting");
     }
+
+    // Getter and Setter Method
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
 }
