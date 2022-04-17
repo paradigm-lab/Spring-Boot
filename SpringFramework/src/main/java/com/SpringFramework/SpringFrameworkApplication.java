1743f183a63d98b2126d15061b9509001c209ade
collins <collinsboniface1000@gmail.com>
2022-04-17 10:06:33 +0300

    Using the AnnotationConfigApplicationContext based with the java approach to configure the beans in the Configuration file

--- SpringFramework/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
+++ SpringFramework/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
@@ -1,17 +1,22 @@
 package com.SpringFramework;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringFrameworkApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		System.out.println("Beans file is loaded");
+		// Configuring the beans in xml approach
+		// ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		// System.out.println("Beans file is loaded");
 
-		Doctor doctor = context.getBean("doctor", Doctor.class);
+		// Configuring the beans in java based approach
+		ApplicationContext  context = new AnnotationConfigApplicationContext(BeanConfig.class);
+		Doctor doctor = context.getBean(Doctor.class);
 		doctor.assist();
+
 	}
 
 }
