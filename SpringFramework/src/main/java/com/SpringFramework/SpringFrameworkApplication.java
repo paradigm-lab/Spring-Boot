41037a762a513422b9857bf1015caae6925c16e1
collins <collinsboniface1000@gmail.com>
2022-04-17 09:11:50 +0300

    Got the bean from the Spring container

--- SpringFramework/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
+++ SpringFramework/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
@@ -8,7 +8,10 @@ public class SpringFrameworkApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext contex = new ClassPathXmlApplicationContext("beans.xml");
+		System.out.println("Beans file is loaded");
 
+		Doctor doctor = (Doctor) contex.getBean("doctor");
+		doctor.assist();
 	}
 
 }
