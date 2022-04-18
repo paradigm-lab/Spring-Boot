98ba867b2e3bf476a82b1bf50f43ed8401815c74
collins <collinsboniface1000@gmail.com>
2022-04-18 10:34:38 +0300

    Getting the bean from the Spring container and used the checkout method

--- AOP/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
+++ AOP/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
@@ -1,9 +1,17 @@
 package com.SpringFramework;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringFrameworkApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+		System.out.println("The Beans Configuration file is loaded");
+
+		ShoppingCart shoppingCart = (ShoppingCart) context.getBean("shoppingcart");
+		shoppingCart.checkout();
 
 	}
 
