210d40c987fd215d94715b609c56b761b340932d
collins <collinsboniface1000@gmail.com>
2022-04-18 10:28:22 +0300

    Removed the SpringBootApplication Annotation

--- AOP/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
+++ AOP/src/main/java/com/SpringFramework/SpringFrameworkApplication.java
@@ -1,13 +1,10 @@
 package com.SpringFramework;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFrameworkApplication.class, args);
+
 	}
 
 }
