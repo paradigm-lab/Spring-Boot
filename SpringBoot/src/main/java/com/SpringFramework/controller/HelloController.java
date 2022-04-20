69de1108f93099fbf2816f6a0b4517fbd9f97cb3
collins <collinsboniface1000@gmail.com>
2022-04-21 00:05:04 +0300

    Replaced the RequestMapping annotation with the @GetMapping annotation

--- SpringBoot/src/main/java/com/SpringFramework/controller/HelloController.java
+++ SpringBoot/src/main/java/com/SpringFramework/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.SpringFramework.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String helloWorld() {
         return "Welcome to kwawingu.com";
     }
