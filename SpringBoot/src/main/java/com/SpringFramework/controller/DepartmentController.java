d7681699b87a5ada4c946bccca58f467e0884576
collins <collinsboniface1000@gmail.com>
2022-04-21 10:57:18 +0300

    Created a handler method with the POST method and got a new object from the spring container from the service layer

--- SpringBoot/src/main/java/com/SpringFramework/controller/DepartmentController.java
+++ SpringBoot/src/main/java/com/SpringFramework/controller/DepartmentController.java
@@ -1,8 +1,23 @@
 package com.SpringFramework.controller;
 
+import com.SpringFramework.entity.Department;
+import com.SpringFramework.service.DepartmentService;
+import com.SpringFramework.service.DepartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
-    
+
+    @Autowired
+    private DepartmentServiceImpl departmentService;
+
+    @RequestMapping(value = "/departments", method = RequestMethod.POST)
+    public Department saveDepartment(@RequestBody Department department) {
+
+        return departmentService.saveDepartment(department);
+    }
 }
