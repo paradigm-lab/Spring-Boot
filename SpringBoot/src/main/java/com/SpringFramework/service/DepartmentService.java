b495a4e785e5240220d73a92baa9b0ff6b503b77
collins <collinsboniface1000@gmail.com>
2022-04-21 10:55:24 +0300

    Added a new method in the Service interface and added the implemenation for the method in my Impl class

--- SpringBoot/src/main/java/com/SpringFramework/service/DepartmentService.java
+++ SpringBoot/src/main/java/com/SpringFramework/service/DepartmentService.java
@@ -1,5 +1,7 @@
 package com.SpringFramework.service;
 
+import com.SpringFramework.entity.Department;
+
 public interface DepartmentService {
-    
+   Department saveDepartment(Department department);
 }
--- SpringBoot/src/main/java/com/SpringFramework/service/DepartmentServiceImpl.java
+++ SpringBoot/src/main/java/com/SpringFramework/service/DepartmentServiceImpl.java
@@ -1,8 +1,19 @@
 package com.SpringFramework.service;
 
+import com.SpringFramework.entity.Department;
+import com.SpringFramework.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
 
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        departmentRepository.save(department);
+        return null;
+    }
 }
