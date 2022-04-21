d29264abb8aef986c21e4c4554ce82937ca0c052
collins <collinsboniface1000@gmail.com>
2022-04-21 11:04:20 +0300

    Using curl as my REST API client to send the data to the DB

---
+++ SpringBoot/test.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+function testAddStudent {
+  BODY=$(cat <<EOF
+{
+  "departmentName": "Computer Science",
+  "departmentAddress": "Tz",
+  "departmentCode": "CS50"
+}
+EOF
+)
+  curl -X POST -H "Content-Type: application/json" -d "$BODY" http://localhost:8082/departments
+}
+
+# | jq
+function testGetStudents {
+  curl -X GET http://localhost:8080/api/v1/student | jq
+}
+
+
+function testDeleteStudent {
+  curl -X DELETE http://localhost:8080/api/v1/student/1
+}
+
+function testPutStudent {
+  curl -X PUT http://localhost:8080/api/v1/student/5 -d name=Maria&email=maria@gmail.com
+}
+
+###################################################################
+
+#testPutStudent
+#testDeleteStudent
+testAddStudent
+#testGetStudents
