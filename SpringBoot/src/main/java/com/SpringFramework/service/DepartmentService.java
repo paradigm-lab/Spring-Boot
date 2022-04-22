517839032ea75dc39ab490e59ab519f2cfc5ef82
collins <collinsboniface1000@gmail.com>
2022-04-22 08:01:57 +0300

    Created a new method in the service layer to find student by name

--- SpringBoot/src/main/java/com/SpringFramework/service/DepartmentService.java
+++ SpringBoot/src/main/java/com/SpringFramework/service/DepartmentService.java
@@ -10,4 +10,5 @@ public interface DepartmentService {
    public Department fetchDepartmentById(Long departmentId);
    public void deleteDepartmentById(Long departmentId);
    public Department updateDepartment(Long departmentId, Department department);
+   public Department fetchDepartmentByName(String departmentName);
 }
