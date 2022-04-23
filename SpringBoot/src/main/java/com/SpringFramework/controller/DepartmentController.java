0d560582ac83589664b60da30c782ee073183afa
collins <collinsboniface1000@gmail.com>
2022-04-23 06:57:55 +0300

    Defined the validation to not blank in both the entity and Controller where the Post is happening

--- SpringBoot/src/main/java/com/SpringFramework/controller/DepartmentController.java
+++ SpringBoot/src/main/java/com/SpringFramework/controller/DepartmentController.java
@@ -6,6 +6,7 @@ import com.SpringFramework.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,7 +16,7 @@ public class DepartmentController {
     private DepartmentServiceImpl departmentService;
 
     @RequestMapping(value = "/departments", method = RequestMethod.POST)
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
 
         return departmentService.saveDepartment(department);
     }
