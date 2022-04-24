81bd0dd56aae5acda0e2d80c36d170e11be1b2d3
collins <collinsboniface1000@gmail.com>
2022-04-24 23:52:21 +0300

    Wrote a test method to test for the ValidDepartmentName

--- SpringBoot/src/test/java/com/SpringFramework/service/DepartmentServiceTest.java
+++ SpringBoot/src/test/java/com/SpringFramework/service/DepartmentServiceTest.java
@@ -1,6 +1,9 @@
 package com.SpringFramework.service;
 
+import com.SpringFramework.entity.Department;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class DepartmentServiceTest {
 
+    @Autowired
+    private DepartmentService departmentService;
+
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound() {
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+        assertEquals(departmentName, found.getDepartmentName());
     }
 }
