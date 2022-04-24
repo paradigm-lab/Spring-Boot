418662f9c0fc3a3e0dfadfb1e86f12a628346db8
collins <collinsboniface1000@gmail.com>
2022-04-24 23:45:05 +0300

    Auto generated the test for the department service layer

---
+++ SpringBoot/src/test/java/com/SpringFramework/service/DepartmentServiceTest.java
@@ -0,0 +1,14 @@
+package com.SpringFramework.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
