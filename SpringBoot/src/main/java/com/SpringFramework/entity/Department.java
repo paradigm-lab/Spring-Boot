2a11a76cb09cea890566b94f1e06774c842cc53c
collins <collinsboniface1000@gmail.com>
2022-04-25 23:03:26 +0300

    Used the mokito Annotation to add the constructor and Builder

--- SpringBoot/src/main/java/com/SpringFramework/entity/Department.java
+++ SpringBoot/src/main/java/com/SpringFramework/entity/Department.java
@@ -1,6 +1,9 @@
 package com.SpringFramework.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +13,9 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
