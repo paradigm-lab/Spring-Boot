6536220c246a4dc4b945850107b801218984d1d0
collins <collinsboniface1000@gmail.com>
2022-04-26 17:58:29 +0300

    Created a new Test package for the repository layer

---
+++ SpringBoot/src/test/java/com/SpringFramework/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.SpringFramework.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
