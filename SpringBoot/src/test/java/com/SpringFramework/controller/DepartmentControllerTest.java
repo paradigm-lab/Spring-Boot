36b2cb5edaa1b85ca459cf3c5dd14ef6a8c38000
collins <collinsboniface1000@gmail.com>
2022-04-26 22:01:52 +0300

    Test FAILED for the saveDepartment method

--- SpringBoot/src/test/java/com/SpringFramework/controller/DepartmentControllerTest.java
+++ SpringBoot/src/test/java/com/SpringFramework/controller/DepartmentControllerTest.java
@@ -1,18 +1,62 @@
 package com.SpringFramework.controller;
 
+import com.SpringFramework.entity.Department;
+import com.SpringFramework.service.DepartmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(DepartmentController.class)
 class DepartmentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DepartmentService departmentService;
+
+    private Department department;
 
     @BeforeEach
     void setUp() {
+        department = Department.builder()
+                .departmentAddress("IAA")
+                .departmentCode("IT-06")
+                .departmentName("IT")
+                .departmentId(1L)
+                .build();
     }
 
     @Test
-    void saveDepartment() {
+    void saveDepartment() throws Exception {
+        Department inputDepartment = Department.builder()
+                .departmentAddress("IAA")
+                .departmentCode("IT-06")
+                .departmentName("IT")
+                .build();
+
+        Mockito.when(departmentService.saveDepartment(inputDepartment))
+                .thenReturn(department);
+
+        mockMvc.perform(post("/departments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "\t\"departmentName\":\"IT\",\n" +
+                                "\t\"departmentAddress\":\"IAA\",\n" +
+                                "\t\"departmentCode\":\"IT-06\",\n" +
+                                "}"))
+                .andExpect(status().isOk());
     }
 
     @Test
