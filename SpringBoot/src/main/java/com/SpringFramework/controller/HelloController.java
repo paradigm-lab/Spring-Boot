15cfdf2ff69fa3ecc031def296123d1eba7a72de
collins <collinsboniface1000@gmail.com>
2022-04-27 23:22:03 +0300

    Loaded the value from the properties file by using the key to get the String

--- SpringBoot/src/main/java/com/SpringFramework/controller/HelloController.java
+++ SpringBoot/src/main/java/com/SpringFramework/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.SpringFramework.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld() {
-        return "Welcome to kwawingu.com";
+        return welcomeMessage;
     }
 
 }
