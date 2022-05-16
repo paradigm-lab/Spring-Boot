0912cef114f728aeaa7a01606bdb0d24c69d6a09
collins <collinsboniface1000@gmail.com>
2022-05-16 22:21:38 +0300

    Defined the download implementation of the Download method

--- FUD/src/main/java/com/filemanagement/controller/AttachmentController.java
+++ FUD/src/main/java/com/filemanagement/controller/AttachmentController.java
@@ -3,7 +3,10 @@ package com.filemanagement.controller;
 import com.filemanagement.entity.Attachment;
 import com.filemanagement.model.ResponseData;
 import com.filemanagement.service.AttachmentService;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -33,9 +36,14 @@ public class AttachmentController {
     }
 
     @GetMapping("/download/{fileId}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) throws Exception {
         Attachment attachment = null;
         attachment = attachmentService.getAttachment(fileId);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(attachment.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + attachment.getFileName() +
+                        "\"")
+                .body(new ByteArrayResource(attachment.getData()));
     }
 
 
