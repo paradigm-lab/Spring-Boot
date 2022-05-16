cd8ab828bfb9041847fa6f28364c34e11ac592de
collins <collinsboniface1000@gmail.com>
2022-05-16 22:00:15 +0300

    Defined the saveAttachment method in the service layer and used the save method to POST the data

--- FUD/src/main/java/com/filemanagement/service/AttachmentService.java
+++ FUD/src/main/java/com/filemanagement/service/AttachmentService.java
@@ -1,4 +1,8 @@
 package com.filemanagement.service;
 
+import com.filemanagement.entity.Attachment;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface AttachmentService {
+    Attachment saveAttachment(MultipartFile file) throws Exception;
 }
--- FUD/src/main/java/com/filemanagement/service/AttachmentServiceImpl.java
+++ FUD/src/main/java/com/filemanagement/service/AttachmentServiceImpl.java
@@ -1,8 +1,31 @@
 package com.filemanagement.service;
 
+import com.filemanagement.entity.Attachment;
+import com.filemanagement.repository.AttachmentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class AttachmentServiceImpl implements AttachmentService {
 
+    private AttachmentRepository attachmentRepository;
+
+    public AttachmentServiceImpl(AttachmentRepository attachmentRepository) {
+        this.attachmentRepository = attachmentRepository;
+    }
+
+    @Override
+    public Attachment saveAttachment(MultipartFile file) throws Exception {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if(fileName.contains("..")) {
+                throw new Exception("Filename contains invalid path sequence" + fileName);
+            }
+            Attachment attachment = new Attachment(fileName, file.getContentType(), file.getBytes());
+            return attachmentRepository.save(attachment);
+        } catch (Exception e) {
+            throw new Exception("Could not save File: " + fileName);
+        }
+    }
 }
