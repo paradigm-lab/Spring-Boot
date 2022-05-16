6992d1dc45fbf42b3deebdd04cd2125387e991bc
collins <collinsboniface1000@gmail.com>
2022-05-16 21:47:53 +0300

    Defined the service layer interface and implementation

---
+++ FUD/src/main/java/com/filemanagement/service/AttachmentService.java
@@ -0,0 +1,4 @@
+package com.filemanagement.service;
+
+public interface AttachmentService {
+}
---
+++ FUD/src/main/java/com/filemanagement/service/AttachmentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.filemanagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttachmentServiceImpl implements AttachmentService {
+
+}
