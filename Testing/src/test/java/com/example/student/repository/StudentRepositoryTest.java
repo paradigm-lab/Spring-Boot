9042cedc91341c6a0d7d1ee2286cc5ce89b1eebc
collins <collinsboniface1000@gmail.com>
2022-05-24 15:15:11 +0300

    Added one more test and tearDown method AfterEach test

--- Testing/src/test/java/com/example/student/repository/StudentRepositoryTest.java
+++ Testing/src/test/java/com/example/student/repository/StudentRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.student.repository;
 
 import com.example.student.entity.Gender;
 import com.example.student.entity.Student;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,8 +15,13 @@ class StudentRepositoryTest {
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
-    void itShouldCheckIfStudentExistsEmail() {
+    void itShouldCheckWhenStudentEmailExists() {
 
         // Given (Call the Service Layer)
         String email = "facade@gmail.com";
@@ -33,4 +39,16 @@ class StudentRepositoryTest {
         assertThat(expected).isTrue();
     }
 
+    @Test
+    void itShouldCheckWhenStudentEmailDoesNotExists() {
+
+        // Given (Call the Service Layer)
+        String email = "facade@gmail.com";
+
+        // When
+        Boolean expected = underTest.selectExistsEmail(email);
+
+        // Then
+        assertThat(expected).isFalse();
+    }
 }
