5299195916d3bec447d908b8fb51def86c7881b5
collins <collinsboniface1000@gmail.com>
2022-05-24 14:15:21 +0300

    Created an api to getallStudent, addStudent, deleteStudent

---
+++ Testing/src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.example.student.controller;
+
+import com.example.student.entity.Student;
+import com.example.student.exception.BadRequestException;
+import com.example.student.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
+public class StudentController {
+
+    @Autowired
+    private final StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@Valid @RequestBody Student student) throws BadRequestException {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+}
