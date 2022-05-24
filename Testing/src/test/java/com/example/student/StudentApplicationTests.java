9feb4dd55b63616dd99d2132db30484c22ef8cdc
collins <collinsboniface1000@gmail.com>
2022-05-24 13:36:23 +0300

    Created an assertThat test to the method

--- Testing/src/test/java/com/example/student/StudentApplicationTests.java
+++ Testing/src/test/java/com/example/student/StudentApplicationTests.java
@@ -2,10 +2,33 @@ package com.example.student;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 public class StudentApplicationTests {
-    @Test
-    void contextLoads() {
 
+    Calculator underTest = new Calculator();
+
+    @Test
+    void itShouldAddTwoNumbers() {
+
+        // BDD
+        // Given
+        int numberOne = 20;
+        int numberTwo = 30;
+
+        // When
+        int result = underTest.add(numberOne, numberTwo);
+
+        // Then
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
     }
+
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
+
 }
