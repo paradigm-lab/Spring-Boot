dba543efc63b63cfd26ef944c2b668382cfcd974
collins <collinsboniface1000@gmail.com>
2022-05-24 15:04:35 +0300

    Update the server port number

--- Testing/src/test/java/com/example/student/repository/StudentRepositoryTest.java
+++ Testing/src/test/java/com/example/student/repository/StudentRepositoryTest.java
@@ -4,10 +4,11 @@ import com.example.student.entity.Gender;
 import com.example.student.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
