ba09515f586c6b4b66a8c92dbad65be475f2f9cf
collins <collinsboniface1000@gmail.com>
2022-06-07 20:59:43 +0300

    Configuring the Spring Security filter chain

---
+++ Security/src/main/java/com/mvc/sec/config/SecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.mvc.sec.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer { // Initialize and Register the Spring Security filter chain
+
+}
---
+++ Security/src/main/java/com/mvc/sec/config/SpringSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.mvc.sec.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity(debug = true) // This annotation will go to create the Spring filter chain
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter { // Help us to create a spring security filter chain
+    // This class is going to help you to create the Spring security filter chain(gunman)
+    // This is a Spring Security Filter chain because it's going to create Spring security filter chain
+    // Now Register the spring security filter chain with our App
+
+
+}
