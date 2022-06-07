d7930361e2507e9f65bab76af878029cc8fb1217
collins <collinsboniface1000@gmail.com>
2022-06-07 17:57:02 +0300

    Updated the dependencies and Controller

--- Security/src/main/java/com/mvc/sec/controller/HelloWorldController.java
+++ Security/src/main/java/com/mvc/sec/controller/HelloWorldController.java
@@ -1,14 +1,17 @@
 package com.mvc.sec.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String helloWorld() {
+    public String helloWorld(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
 
         return "hello-world";
     }
