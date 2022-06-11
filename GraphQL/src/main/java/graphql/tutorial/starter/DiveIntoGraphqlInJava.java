6d0476f54b8b65b06e959a1bd0ed834a25e7f4d4
collins <collinsboniface1000@gmail.com>
2022-06-11 13:23:05 +0300

    Defined the Database Layer or the POJO

---
+++ GraphQL/src/main/java/graphql/tutorial/starter/DiveIntoGraphqlInJava.java
@@ -0,0 +1,8 @@
+package graphql.tutorial.starter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveIntoGraphqlInJava {
+
+}
---
+++ GraphQL/src/main/java/graphql/tutorial/starter/pojo/SpeakerTalk.java
@@ -0,0 +1,19 @@
+package graphql.tutorial.starter.pojo;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class SpeakerTalk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long speakerId;
+    private Long talkId;
+}
