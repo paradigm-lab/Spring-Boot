bc1ac9f56c77a27242e9a500c952467a91a592b4
collins <collinsboniface1000@gmail.com>
2022-06-11 13:41:29 +0300

    Defined the psvm method

--- GraphQL/src/main/java/graphql/tutorial/starter/DiveIntoGraphqlInJava.java
+++ GraphQL/src/main/java/graphql/tutorial/starter/DiveIntoGraphqlInJava.java
@@ -1,8 +1,11 @@
 package graphql.tutorial.starter;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiveIntoGraphqlInJava {
-
+    public static void main(String[] args) {
+        SpringApplication.run(DiveIntoGraphqlInJava.class, args);
+    }
 }
