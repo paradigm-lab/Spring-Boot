6f40907d0ac1bf95fc5f7f8b415cd0656c8c234a
collins <collinsboniface1000@gmail.com>
2022-06-11 13:55:32 +0300

    Updated the Repository layer

--- GraphQL/src/main/java/graphql/tutorial/starter/repository/AttendeeTalkRepository.java
+++ GraphQL/src/main/java/graphql/tutorial/starter/repository/AttendeeTalkRepository.java
@@ -3,5 +3,9 @@ package graphql.tutorial.starter.repository;
 import graphql.tutorial.starter.pojo.AttendeeTalk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeTalkRepository extends JpaRepository<AttendeeTalk, Long> {
+    List<AttendeeTalk> findAllByAttendeeId(Long attendeeId);
+    List<AttendeeTalk> findAllByTalkId(Long talkId);
 }
--- GraphQL/src/main/java/graphql/tutorial/starter/repository/SpeakerTalkRepository.java
+++ GraphQL/src/main/java/graphql/tutorial/starter/repository/SpeakerTalkRepository.java
@@ -3,5 +3,9 @@ package graphql.tutorial.starter.repository;
 import graphql.tutorial.starter.pojo.SpeakerTalk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpeakerTalkRepository extends JpaRepository<SpeakerTalk, Long> {
+    List<SpeakerTalk> findAllBySpeakerId(Long speakerId);
+    List<SpeakerTalk> findAllByTalkId(Long talkId);
 }
