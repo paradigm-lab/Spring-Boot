ca62ec3f9c9568c182be4318524ba7ac94dde014
collins <collinsboniface1000@gmail.com>
2022-06-15 09:08:48 +0300

    Created a Rest endpoint

---
+++ Security/src/main/java/com/mvc/sec/controller/MyRestController.java
@@ -0,0 +1,16 @@
+package com.mvc.sec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+// @Controller
+// @ResponseBody // Returns directly as a response (No jsp page will be searched by this name)
+@RestController
+public class MyRestController {
+
+    @RequestMapping(value = "/greet/{yourName}", method = RequestMethod.GET)
+    public String greet(@PathVariable("yourName") String yourName) {
+
+        return "Good Evening " + yourName;
+    }
+}
