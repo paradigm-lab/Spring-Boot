73cd17ec8600f5e3d28b981c707660ea10fc2d6e
collins <collinsboniface1000@gmail.com>
2022-06-15 10:42:09 +0300

    Overrided the configure method and Gave a basic Authenctication

--- Security/src/main/java/com/mvc/sec/config/SpringSecurityConfig.java
+++ Security/src/main/java/com/mvc/sec/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mvc.sec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -35,5 +36,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter { // Help
         return NoOpPasswordEncoder.getInstance();
     }
      */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();   // Basic Authentication the encoded password can be decoded (Base64)
+    }
 
 }
