2d6e7b8a600f66f4ce96e53fbdde47b5f4421a42
collins <collinsboniface1000@gmail.com>
2022-07-04 11:46:22 +0300

    Updated the variable name

--- Security/src/main/java/com/mvc/sec/controller/MyRestController.java
+++ Security/src/main/java/com/mvc/sec/controller/MyRestController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 public class MyRestController {
 
     @RequestMapping(value = "/greet/{yourName}", method = RequestMethod.GET)
-    public String greet(@PathVariable("yourName") String yourName) {
+    public String greet(@PathVariable("yourName") String name) {
 
-        return "Good Evening " + yourName;
+        return "Good Evening " + name;
     }
 }
