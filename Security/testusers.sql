6eb507c5efe5d3a5fcd993a62e5a92a1e087f30f
collins <collinsboniface1000@gmail.com>
2022-07-06 15:09:38 +0300

    More user to authorities table

--- Security/testusers.sql
+++ Security/testusers.sql
@@ -1,5 +1,15 @@
 -- Users to test in the database
 
+/*
 INSERT INTO users (username, password, enabled) values ('matt', 'password', 'True');
 INSERT INTO users (username, password, enabled) values ('collin', 'password', 'True');
 INSERT INTO users (username, password, enabled) values ('kwawingu', 'password', 'True');
+*/
+
+-- authorities table
+INSERT INTO authorities (username, authority) values ('matt', 'ADMIN');
+INSERT INTO authorities (username, authority) values ('matt', 'USER');
+
+INSERT INTO authorities (username, authority) values ('collin', 'USER');
+INSERT INTO authorities (username, authority) values ('kwawingu', 'USER');
+
