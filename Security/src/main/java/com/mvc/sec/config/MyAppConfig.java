037dce010d2d39974669ab79e61f0e812dd3d60e
collins <collinsboniface1000@gmail.com>
2022-07-06 15:37:34 +0300

    Defined the Bean DataSource

--- Security/src/main/java/com/mvc/sec/config/MyAppConfig.java
+++ Security/src/main/java/com/mvc/sec/config/MyAppConfig.java
@@ -2,9 +2,12 @@ package com.mvc.sec.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -21,6 +24,22 @@ public class MyAppConfig {
         TemplateResolver.setCheckExistence(true);
 
         return TemplateResolver;
+    }
+
+
+    // How can we connect to db: Datasource
+    @Bean
+    public DataSource datasource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setUsername("security");
+        driverManagerDataSource.setPassword("security");
+
+        // The current Database used for the project
+        driverManagerDataSource.setUrl("jdbc:postgresql://postgres-standalone:5432/springsec");
+        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+
+        return driverManagerDataSource;
     }
 
 }
