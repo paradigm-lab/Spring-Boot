33665495855bd18b568e226f36bd86a2ea4ee84a
collins <collinsboniface1000@gmail.com>
2022-07-06 15:09:04 +0300

    Created a new table for the authorities

--- Security/database.sql
+++ Security/database.sql
@@ -1,5 +1,6 @@
 -- Note: The Database name should be in small case letter.
 
+/*
 CREATE DATABASE springsec;
 
 CREATE USER security WITH ENCRYPTED PASSWORD 'security';
@@ -16,3 +17,15 @@ CREATE TABLE users (
 	password VARCHAR(50) NOT NULL,
 	enabled BOOLEAN NOT NULL
 );
+*/
+
+-- This table will store the roles/authories
+CREATE TABLE authorities (
+	username VARCHAR(50) NOT NULL,
+	authority VARCHAR(50) NOT NULL, -- Roles
+	CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username) -- A foreign key to the users table
+);
+
+
+-- Index
+-- CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority); 
