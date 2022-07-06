ea725c49c37a4ba42db99c2cf9189c1be42e310e
collins <collinsboniface1000@gmail.com>
2022-07-06 14:27:43 +0300

    Created the test user to be insert in the Database

---
+++ Security/testusers.sql
@@ -0,0 +1,5 @@
+-- Users to test in the database
+
+INSERT INTO users (username, password, enabled) values ('matt', 'password', 'True');
+INSERT INTO users (username, password, enabled) values ('collin', 'password', 'True');
+INSERT INTO users (username, password, enabled) values ('kwawingu', 'password', 'True');
